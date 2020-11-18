f9d18ba0352a33b5a30936b7e856df01c52ae2c4
luke john <luke0145555@hotmail.co.uk>
2020-11-18 00:31:49 +0000

    create a poll within each document, map the polls for the page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,58 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { dbStore, auth } from '../services/firebase';
+import React, { useState, useEffect } from "react";
+import { dbStore, auth } from "../services/firebase";
 import { v4 as uuidv4 } from "uuid";
 
 function HomePage() {
+  const currentPage = window.location.pathname.split("/")[1];
 
-    const currentPage = window.location.pathname.split("/")[1]
-
-  const [pages, setPages] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [authenticated, setAuthenticated] = useState(false)
+  const [pages, setPages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
   const [desc, setDesc] = useState("");
+  const [subCollection, setSubCollection] = useState("");
 
-  const ref = dbStore.collection("pages")
-
+  const ref = dbStore.collection("pages");
 
   // get database stuff real time
   function getPages() {
     setLoading(true)
-    ref.onSnapshot((querySnapshot) => {
+    dbStore.collection("pages").onSnapshot((querySnapshot) => {
       const items = []
       querySnapshot.forEach((doc) => {
-          console.log(doc.data, "item")
-            items.push(doc.data())
-      })
+        items.push(doc.data())
+      });
+      console.log(items, "1234")
       const pageExists = items.filter((page) => {
-          return page.title === currentPage
-      })
+        return page.title === currentPage;
+      });
       setPages(pageExists)
       setLoading(false)
-    })
+    });
   }
 
-  useEffect (() => {
-    getPages()
+  function getSubCollection() {
+    setLoading(true);
+    dbStore
+      .collection("pages")
+      .doc(currentPage)
+      .collection("polls")
+      .onSnapshot((querySnapshot) => {
+        const items = []
+        querySnapshot.forEach((doc) => {
+          console.log(doc.data(), "subcolData")
+          items.push(doc.data())
+        });
+        setSubCollection(items)
+        setLoading(false)
+      });
+  }
+
+  useEffect(() => {
+    getSubCollection();
+    getPages();
     //getPages2()
     auth().onAuthStateChanged((user) => {
-            if (user) {
-        setLoading(false)
-        setAuthenticated(true)
-        } else {
-        setLoading(false)
-        setAuthenticated(false)
+      if (user) {
+        setLoading(false);
+        setAuthenticated(true);
+      } else {
+        setLoading(false);
+        setAuthenticated(false);
       }
-    })
-  }, [])
+    });
+  }, []);
 
   function addPage(newPage) {
     ref
       //.doc() use if for some reason you want that firestore generates the id
-      .doc(newPage.id)
+      .doc(currentPage)
       .set(newPage)
       .catch((err) => {
         console.error(err);
       });
+
+      const cityRef = dbStore.collection('pages').doc(currentPage).collection('polls')
+
+      cityRef.add({
+        question: 'Will this subreddit take off?',
+        yay: 0,
+        nay: 0,
+        id: uuidv4()
+    }, { merge: true })
   }
 
   //DELETE FUNCTION
@@ -67,7 +93,6 @@ function HomePage() {
 
   // EDIT FUNCTION
   function editPage(updatedPage) {
-    setLoading();
     ref
       .doc(updatedPage.id)
       .update(updatedPage)
@@ -75,34 +100,43 @@ function HomePage() {
         console.error(err);
       });
   }
-  if(loading) {
-    return <h1> loading... </h1>
+  if (loading) {
+    return <h1> loading... </h1>;
   }
-    return  (
-     <div>
-         {pages.length === 0 
-         ? <div className="inputContainer">
-                    This page doesn't exist, want to start a subforum?
-
-        <textarea value={desc} onChange={(e) => setDesc(e.target.value)} />
-        <button onClick={() => addPage({ title: currentPage, desc, id: uuidv4() })}>
-          Submit
-        </button>
-      </div>
-         : <div>
-         {pages.map((page) => (
-         <div key={page.id}>
-             <h4>{page.title}</h4>
-             <div>{page.desc}</div>
-         <div>
-           </div>
-         </div>
-        ))}
+  console.log(subCollection, "1234456sub")
+  return (
+    <div>
+      {pages.length === 0 ? (
+        <div className="inputContainer">
+          This page doesn't exist, want to start a subforum?
+          <textarea value={desc} onChange={(e) => setDesc(e.target.value)} />
+          <button
+            onClick={() => addPage({ title: currentPage, desc, id: uuidv4() })}
+          >
+            Submit
+          </button>
         </div>
-         }
-
-     </div>
-    )
+      ) : (
+        <div>
+          {pages.map((page) => (
+            <div key={page.id}>
+              <h4>{page.title}</h4>
+              <div>{page.desc}</div>
+              <div></div>
+            </div>
+          ))}
+          {subCollection.map((subCollection) => (
+            <div key={subCollection.id}>
+              <h4>{subCollection.question}</h4>
+              <div> yay: {subCollection.yay}</div>
+              <div> nay: {subCollection.nay}</div>
+              <div></div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default HomePage;
